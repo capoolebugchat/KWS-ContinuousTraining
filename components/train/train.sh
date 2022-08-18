751045e3258d171337d423a60e7f191437076217
anhquan.le52 <anhquan.le52@gmail.com>
2022-08-18 17:52:44 +0700

    end of day commit, havent done much besides experiementing with minio client SDK and rclone cli

--- components/train/train.sh
+++ components/train/train.sh
@@ -1,12 +1,12 @@
-#training phase
-python $1 --yaml-path=$2 --env-path=$3 --data-path=$4
+#pre-training 
+python yaml_to_env.py --yaml-path=$2 --env-path=$3 --data-path=$4
 pip install -r requirements.txt --no-cache-dir
 echo "Hyperparams loaded"
 
-pip install tensorflow==2.7.0
-pip install -U numpy
-cd components/train
+#training using provided script
 python -m kws_streaming.train.model_train_eval --alsologtostderr ds_tc_resnet
 echo "Trained"
 
-#deployment phase
+#uploading to minio
+
+
