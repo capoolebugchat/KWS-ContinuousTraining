890acb18707b067842314fd436a997bb9e64d188
anhquan.le52 <anhquan.le52@gmail.com>
2022-08-19 11:21:44 +0700

    update

--- components/train/train.sh
+++ components/train/train.sh
@@ -1,4 +1,10 @@
-#pre-training 
+#pre-training
+#script's args:
+# $1: yaml config file path
+# $2: env path( ALWAYS IS hparam.env)
+# $3: dataset dir
+# $4: 
+
 python yaml_to_env.py --yaml-path=$1 --env-path=$2 --data-path=$3
 pip install -r requirements.txt --no-cache-dir
 echo "Hyperparams loaded"
@@ -8,5 +14,5 @@ python -m kws_streaming.train.model_train_eval --alsologtostderr ds_tc_resnet
 echo "Trained"
 
 #uploading to minio
-
-
+echo "uploading train result to Minio"
+# python upload_to_minio.py --local-dir="train_res/ds_tc_resnet" --bucket-name=model-store --remote-dir=$4
