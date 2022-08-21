70456c3d5f30bac6a4a5ce027a36d8ae96868a38
anhquan.le52 <anhquan.le52@gmail.com>
2022-08-21 12:08:56 +0700

    broken code do not use

--- components/train/build_train.sh
+++ components/train/build_train.sh
@@ -1,9 +1,8 @@
 #build training image
 train_image_name=capoolebugchat/kws-training
-train_image_tag=v0.1.0
+train_image_tag=v0.4.0
 train_full_image_name=${train_image_name}:${train_image_tag}
 
-cd "$(dirname "$0")" 
 docker build -t "${train_full_image_name}" .
 docker push "$train_full_image_name"
 
---
+++ components/train/compile_train.py
@@ -0,0 +1,82 @@
+from collections import namedtuple
+import os
+import argparse
+from minio import Minio
+import glob
+import yaml
+import logging
+import kfp
+import kfp.v2.dsl as dsl
+from kfp.v2.dsl import (
+    Input, Output, Artifact, Model, Dataset,component)
+from typing import NamedTuple
+
+@component
+def train(
+    dataset: Input[Dataset],
+    config: Input[Artifact],
+    model: Input[Model],
+    bucket_name: str
+)->namedtuple("model", [Model]):
+
+    minio_client = Minio(
+        "minio-service.kubeflow.svc.cluster.local:9000",
+        access_key="minio",
+        secret_key="minio123",
+        secure=False
+    )
+
+    def _yaml_to_env(yaml_file, env_file, data_path):
+    
+        yaml_f = open(yaml_file,'r')
+        env_f = open(env_file,'w')
+        hyperparams = yaml.safe_load(yaml_f)
+        hyperparams['data_path'] = data_path
+        logging.info("Loading hyperparams:")
+        print("Loading hyperparams:")
+        for key in hyperparams:
+            logging.info(f"{key} = {hyperparams[key]}")
+            print(f"{key} = {hyperparams[key]}")
+            if isinstance(hyperparams[key], str):
+                env_f.write(f"{key} = '{hyperparams[key]}'\n")
+            else: env_f.write(f"{key} = {hyperparams[key]}\n")
+
+    def _train():
+        os.system("python -m kws_streaming.train.model_train_eval ds_tc_resnet --alsologtostderr")
+
+    def _upload_local_directory_to_minio(local_path, bucket_name, minio_path):
+        assert os.path.isdir(local_path)
+
+        for local_file in glob.glob(local_path + '/**'):
+            local_file = local_file.replace(os.sep, "/") # Replace \ with / on Windows
+            if not os.path.isfile(local_file):
+                _upload_local_directory_to_minio(
+                    local_file, bucket_name, minio_path + "/" + os.path.basename(local_file))
+            else:
+                remote_path = os.path.join(
+                    minio_path, local_file[1 + len(local_path):])
+                remote_path = remote_path.replace(
+                    os.sep, "/")  # Replace \ with / on Windows
+                minio_client.fput_object(bucket_name, remote_path, local_file)
+
+    model.metadata["bucket"] = bucket_name
+    
+    _yaml_to_env(config.path, "hparam.env", dataset.path)
+    _train()
+    _upload_local_directory_to_minio(
+        "./train_res/",model.metadata["bucket"],model.path)
+
+    model.path = bucket_name+"/test-res"
+    
+    model_ = NamedTuple("model", [("model",Model)])
+
+    return model_(model=model)
+
+from kfp.components import create_component_from_func_v2
+if __name__ == "__main__":
+    kfp.v2.dsl.component(
+        func=train,
+        base_image="docker.io/capoolebugchat/kws-training:v0.4.0",
+        output_component_file="component_v2.yaml"
+    )
+    
